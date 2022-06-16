825f054cdfd500407d8fe8eed7de8c8d2ec01218
TBomberman <TBomberman@users.noreply.github.com>
2022-06-16 12:02:18 -0700

    Connect modal drop town

--- src/components/OpenLoan/OpenLoan.tsx
+++ src/components/OpenLoan/OpenLoan.tsx
@@ -1,24 +1,20 @@
 import * as React from 'react';
-import { Token, Tokens } from '../SelectToken/Token';
-
+import { Token } from '../SelectToken/Token';
+import SelectTokenModal from '../SelectToken/SelectTokenModal';
+import { useDisclosure } from "@chakra-ui/hooks"
 import {
     Box,
     Container,
     FormControl,
     Heading,
-    List,
-    ListItem,
-    Center,
     FormLabel,
     Select,
     Button,
     ButtonGroup,
-    Icon,
     Text,
     VStack,
-    Input
+    Input,
 } from '@chakra-ui/react';
-
 import {
     FaInfoCircle,
 } from 'react-icons/fa';
@@ -28,6 +24,20 @@ interface OpenLoanProps {
 }
 
 const OpenLoan: React.FC<OpenLoanProps> = (props) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [ tokenNumber, setTokenNumber ] = React.useState(0);
+    const handleTokenSelected = (token: Token, tokenNumber: number) => {
+        console.log("selected token" + tokenNumber + " " + token.symbol)
+    }
+    function onOpenToken0() {
+        setTokenNumber(0);
+        onOpen();
+    }
+    function onOpenToken1() {
+        setTokenNumber(1);
+        onOpen();
+    }
+
     return (
         <Container>
             <Box borderRadius={'2xl'} bg={'#1d2c52'} maxW='450px' boxShadow='dark-lg'>
@@ -36,8 +46,9 @@ const OpenLoan: React.FC<OpenLoanProps> = (props) => {
                         <Heading color={'#e2e8f0'} marginBottom={'25px'}>Open Your Loan</Heading>
                         <FormLabel variant='openLoan'>Select a Token Pair</FormLabel>
                         <Container>
-                            <Container w='50%' display='inline-block' ><Select color={'#e2e8f0'} placeholder='Select a token'/></Container>
-                            <Container w='50%' display='inline-block' ><Select color={'#e2e8f0'} placeholder='Select a token'/></Container>
+                            <Container w='50%' display='inline-block' ><Select id='token0' onClick={onOpenToken0} color={'#e2e8f0'} placeholder='Select a token'/></Container>
+                            <Container w='50%' display='inline-block' ><Select id='token1' onClick={onOpenToken1} color={'#e2e8f0'} placeholder='Select a token'/></Container>
+                            <SelectTokenModal handleTokenSelected={handleTokenSelected} isOpen={isOpen} onClose={onClose} tokenNumber={tokenNumber}></SelectTokenModal>
                         </Container>
                         <Container textAlign='center'>
                             <ButtonGroup variant='loanInfo' display='inline-block' size='tiny' >
