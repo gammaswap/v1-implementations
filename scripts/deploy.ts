da9a468809675ba54ed0ddb35bcfe288c45edfc3
TBomberman <TBomberman@users.noreply.github.com>
2022-08-21 11:42:10 -0700

    Deploy core factory contract locally

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,23 +3,54 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+const UniswapV2FactoryJSON = require("@uniswap/v2-core/build/UniswapV2Factory.json");
 import { ethers } from "hardhat";
+import type { TestERC20 } from "../typechain/TestERC20";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+  const TestERC20Contract = await ethers.getContractFactory("TestERC20");
+  const tokenA = await TestERC20Contract.deploy("Test Token A", "TOKA");
+  const tokenB = await TestERC20Contract.deploy("Test Token B", "TOKB");
+  const tokenC = await TestERC20Contract.deploy("Test Token C", "TOKC");
+  const WETH = await TestERC20Contract.deploy("WETH", "WETH");
+  await tokenA.deployed();
+  await tokenB.deployed();
+  await tokenC.deployed();
+  await WETH.deployed();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const UniswapV2Factory = new ethers.ContractFactory(
+    UniswapV2FactoryJSON.abi,
+    UniswapV2FactoryJSON.bytecode,
+    owner
+  );
+  const uniFactory = await UniswapV2Factory.deploy(owner.address);
+  await uniFactory.deployed();
+  console.log("UniswapV2Factory Address >> " + uniFactory.address);
 
-  await greeter.deployed();
+  async function createPair(token1: TestERC20, token2: TestERC20) {
+    await uniFactory.createPair(token1.address, token2.address);
+    const uniPairAddress: string = await uniFactory.getPair(
+      token1.address,
+      token2.address
+    );
+    const token1Symbol = await token1.symbol();
+    const token2Symbol = await token2.symbol();
+    console.log(token1Symbol + "/" + token2Symbol + " uniPairAddress >> "
+      + uniPairAddress);
+  }
 
-  console.log("Greeter deployed to:", greeter.address);
+  await createPair(tokenA, tokenB);
+  await createPair(tokenA, tokenC);
+  await createPair(tokenB, tokenC);
+  await createPair(tokenA, WETH);
+  await createPair(tokenB, WETH);
+  await createPair(tokenC, WETH);
+  
+  const GammaPoolFactory = await ethers.getContractFactory("GammaPoolFactory");
+  const factory = await GammaPoolFactory.deploy(owner.address);
+  await factory.deployed()
+  console.log("GammaPoolFactory Address >> " + factory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -28,3 +59,4 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
