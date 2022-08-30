95ad8d4c8661ddac680863cfea57208765907728
TBomberman <TBomberman@users.noreply.github.com>
2022-08-29 23:56:08 -0700

    Initial commit for strategies tests.

---
+++ test/strategies/cpmm/CPMMLongStrategy.test.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+describe("CPMM Long Strategy", function () {
+  
+  beforeEach(async function () {
+  });
+
+  // You can nest describe calls to create subsections.
+  context("First Context", function () {
+    it("Should do something", async function () {
+    });
+  });
+
+  context("Second Context", function () {
+    it("Should do something", async function () {
+    });
+  });
+});
