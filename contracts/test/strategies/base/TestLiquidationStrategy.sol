c27f52f22cdae4ca465524579bbec5e9afdb811d
TBomberman <TBomberman@users.noreply.github.com>
2022-11-22 06:25:48 -0800

    Liquidation Tests without value checking

---
+++ contracts/test/strategies/base/TestLiquidationStrategy.sol
@@ -0,0 +1,204 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.4;
+
+import "../base/TestLongStrategy.sol";
+import "../../../strategies/base/LiquidationStrategy.sol";
+
+contract TestLiquidationStrategy is LiquidationStrategy { //TestLongStrategy, 
+    using LibStorage for LibStorage.Storage;
+    event LoanCreated(address indexed caller, uint256 tokenId);
+    uint80 public borrowRate = 1;
+    uint16 public origFee = 0;
+    uint16 public protocolId;
+
+    function initialize(address cfmm, uint16 _protocolId, address[] calldata tokens) external virtual {
+        protocolId = _protocolId;
+        s.initialize(msg.sender, cfmm, tokens);
+    }
+
+    function tokens() public virtual view returns(address[] memory) {
+        return s.tokens;
+    }
+
+    function tokenBalances() public virtual view returns(uint128[] memory) {
+        return s.TOKEN_BALANCE;
+    }
+
+    // **** LONG GAMMA **** //
+    function createLoan() external virtual returns(uint256 tokenId) {
+        tokenId = s.createLoan(s.tokens.length);
+        emit LoanCreated(msg.sender, tokenId);
+    }
+
+    function getLoan(uint256 tokenId) public virtual view returns(uint256 id, address poolId, uint128[] memory tokensHeld,
+        uint256 heldLiquidity, uint256 initLiquidity, uint256 liquidity, uint256 lpTokens, uint256 rateIndex) {
+        LibStorage.Loan storage _loan = _getLoan(tokenId);
+        id = _loan.id;
+        poolId = _loan.poolId;
+        tokensHeld = _loan.tokensHeld;
+        heldLiquidity = calcInvariant(s.cfmm, _loan.tokensHeld);
+        initLiquidity = _loan.initLiquidity;
+        liquidity = _loan.liquidity;
+        lpTokens = _loan.lpTokens;
+        rateIndex = _loan.rateIndex;
+    }
+
+    function setLiquidity(uint256 tokenId, uint128 liquidity) public virtual {
+        LibStorage.Loan storage _loan = _getLoan(tokenId);
+        _loan.liquidity = liquidity;
+    }
+
+    function setHeldAmounts(uint256 tokenId, uint128[] calldata heldAmounts) public virtual {
+        LibStorage.Loan storage _loan = _getLoan(tokenId);
+        _loan.tokensHeld = heldAmounts;
+    }
+
+    function checkMargin(uint256 tokenId, uint256 limit) public virtual view returns(bool) {
+        LibStorage.Loan storage _loan = _getLoan(tokenId);
+        checkMargin(_loan, limit);
+        return true;
+    }
+
+    function setBorrowRate(uint80 _borrowRate) public virtual {
+        borrowRate = _borrowRate;
+    }
+
+    function calcBorrowRate(uint256 lpInvariant, uint256 borrowedInvariant) internal virtual override view returns(uint256) {
+        return borrowRate;
+    }
+
+    //LongGamma
+    function beforeRepay(LibStorage.Loan storage _loan, uint256[] memory amounts) internal virtual override {
+        _loan.tokensHeld[0] -= uint128(amounts[0]);
+        _loan.tokensHeld[1] -= uint128(amounts[1]);
+    }
+
+    function depositToCFMM(address cfmm, uint256[] memory amounts, address to) internal virtual override returns(uint256 liquidity) {
+        liquidity = uint128(amounts[0]);
+        TestCFMM(cfmm).mint(liquidity / 2, address(this));
+    }
+
+    function calcTokensToRepay(uint256 liquidity) internal virtual override view returns(uint256[] memory amounts) {
+        amounts = new uint256[](2);
+        amounts[0] = liquidity;
+        amounts[1] = liquidity * 2;
+    }
+
+    function squareRoot(uint256 num) public virtual pure returns(uint256) {
+        return Math.sqrt(num * (10**18));
+    }
+
+    function beforeSwapTokens(LibStorage.Loan storage _loan, int256[] calldata deltas) internal virtual override view returns(uint256[] memory outAmts, uint256[] memory inAmts){
+        outAmts = new uint256[](2);
+        inAmts = new uint256[](2);
+        outAmts[0] =  deltas[0] > 0 ? 0 : uint256(-deltas[0]);
+        outAmts[1] =  deltas[1] > 0 ? 0 : uint256(-deltas[1]);
+        inAmts[0] = deltas[0] > 0 ? uint256(deltas[0]) : 0;
+        inAmts[1] = deltas[1] > 0 ? uint256(deltas[1]) : 0;
+    }
+
+    function swapTokens(LibStorage.Loan storage _loan, uint256[] memory outAmts, uint256[] memory inAmts) internal virtual override {
+        address cfmm = s.cfmm;
+
+        if(outAmts[0] > 0) {
+            GammaSwapLibrary.safeTransfer(IERC20(s.tokens[0]), cfmm, outAmts[0]);
+        } else if(outAmts[1] > 0) {
+            GammaSwapLibrary.safeTransfer(IERC20(s.tokens[1]), cfmm, outAmts[1]);
+        }
+
+        if(inAmts[0] > 0) {
+            TestERC20(s.tokens[0]).mint(address(this), inAmts[0]);
+        } else if(inAmts[1] > 0) {
+            TestERC20(s.tokens[1]).mint(address(this), inAmts[1]);
+        }
+    }
+
+    //BaseStrategy
+    function updateReserves() internal override virtual {
+    }
+
+    function calcInvariant(address cfmm, uint128[] memory amounts) internal virtual override view returns(uint256) {
+        return Math.sqrt(uint256(amounts[0]) * amounts[1]);
+    }
+
+    function withdrawFromCFMM(address cfmm, address to, uint256 amount) internal virtual override returns(uint256[] memory amounts) {
+        amounts = new uint256[](2);
+        amounts[0] = amount * 2;
+        amounts[1] = amount * 4;
+    }
+
+    function testOpenLoan(uint256 tokenId, uint256 lpTokens) public virtual {
+        openLoan(_getLoan(tokenId), lpTokens);
+    }
+
+    function testPayLoan(uint256 tokenId, uint256 liquidity) public virtual {
+        LibStorage.Loan storage _loan = _getLoan(tokenId);
+        payLoan(_loan, liquidity);
+    }
+
+    function updateLoan(LibStorage.Loan storage _loan) internal override {
+        uint96 rateIndex = borrowRate;
+        updateLoanLiquidity(_loan, rateIndex);
+    }
+
+    function setLPTokenLoanBalance(uint256 tokenId, uint256 lpInvariant, uint256 lpTokenBalance, uint256 liquidity, uint256 lpTokens, uint256 lastCFMMInvariant, uint256 lastCFMMTotalSupply) public virtual {
+        LibStorage.Loan storage _loan = _getLoan(tokenId);
+
+        s.LP_INVARIANT = uint128(lpInvariant);
+        s.LP_TOKEN_BALANCE = lpTokenBalance;
+
+        s.BORROWED_INVARIANT = uint128(liquidity);
+        s.LP_TOKEN_BORROWED = lpTokens;
+        s.LP_TOKEN_BORROWED_PLUS_INTEREST = lpTokens;
+
+        //s.TOTAL_INVARIANT = s.LP_INVARIANT + s.BORROWED_INVARIANT;
+        //s.LP_TOKEN_TOTAL = s.LP_TOKEN_BALANCE + s.LP_TOKEN_BORROWED_PLUS_INTEREST;
+
+        s.lastCFMMInvariant = uint128(lastCFMMInvariant);
+        s.lastCFMMTotalSupply = lastCFMMTotalSupply;
+
+        _loan.liquidity = uint128(liquidity);
+        _loan.lpTokens = lpTokens;
+    }
+
+    function setLPTokenBalance(uint256 lpInvariant, uint256 lpTokenBalance, uint256 lastCFMMInvariant, uint256 lastCFMMTotalSupply) public virtual {
+        s.LP_TOKEN_BALANCE = lpTokenBalance;
+        //s.LP_TOKEN_TOTAL = lpTokenBalance;
+        s.LP_INVARIANT = uint128(lpInvariant);
+        //s.TOTAL_INVARIANT = lpInvariant;
+        s.lastCFMMInvariant = uint128(lastCFMMInvariant);
+        s.lastCFMMTotalSupply = lastCFMMTotalSupply;
+    }
+
+    function chargeLPTokenInterest(uint256 tokenId, uint256 lpTokenInterest) public virtual {
+        LibStorage.Loan storage _loan = _getLoan(tokenId);
+
+        uint128 invariantInterest = uint128(lpTokenInterest * s.LP_INVARIANT / s.LP_TOKEN_BALANCE);
+        _loan.liquidity = _loan.liquidity + invariantInterest;
+        s.BORROWED_INVARIANT = s.BORROWED_INVARIANT + invariantInterest;
+        //s.TOTAL_INVARIANT = s.TOTAL_INVARIANT + invariantInterest;
+
+        s.LP_TOKEN_BORROWED_PLUS_INTEREST = s.LP_TOKEN_BORROWED_PLUS_INTEREST + lpTokenInterest;
+        //s.LP_TOKEN_TOTAL = s.LP_TOKEN_TOTAL + lpTokenInterest;
+    }
+
+    function getLoanChangeData(uint256 tokenId) public virtual view returns(uint256 loanLiquidity, uint256 loanLpTokens,
+        uint256 borrowedInvariant, uint256 lpInvariant, uint256 totalInvariant,
+        uint256 lpTokenBorrowed, uint256 lpTokenBalance, uint256 lpTokenBorrowedPlusInterest,
+        uint256 lpTokenTotal, uint256 lastCFMMInvariant, uint256 lastCFMMTotalSupply) {
+        LibStorage.Loan storage _loan = _getLoan(tokenId);
+
+        return(_loan.liquidity, _loan.lpTokens,
+            s.BORROWED_INVARIANT, s.LP_INVARIANT, (s.BORROWED_INVARIANT + s.LP_INVARIANT),
+            s.LP_TOKEN_BORROWED, s.LP_TOKEN_BALANCE, s.LP_TOKEN_BORROWED_PLUS_INTEREST,
+            (s.LP_TOKEN_BALANCE + s.LP_TOKEN_BORROWED_PLUS_INTEREST), s.lastCFMMInvariant, s.lastCFMMTotalSupply);
+    }
+
+    function setOriginationFee(uint16 _origFee) external virtual {
+        origFee = _origFee;
+    }
+
+    function originationFee() internal override virtual view returns(uint16) {
+        return origFee;
+    }
+}
---
+++ test/strategies/base/LiquidationStrategy.test.ts
@@ -0,0 +1,154 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { BigNumber } from "ethers";
+
+const PROTOCOL_ID = 1;
+
+describe("LiquidationStrategy", function () {
+  let TestERC20: any;
+  let TestCFMM: any;
+  let TestLiquidationStrategy: any;
+  let tokenA: any;
+  let tokenB: any;
+  let cfmm: any;
+  let liquidationStrategy: any;
+  let owner: any;
+  let addr1: any;
+  let addr2: any;
+  let protocol: any;
+  const ONE = BigNumber.from(10).pow(18);
+  const TWO = BigNumber.from(10).pow(18).mul(2);
+  const FOUR = BigNumber.from(10).pow(18).mul(4);
+
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    TestERC20 = await ethers.getContractFactory("TestERC20");
+    [owner, addr1, addr2] = await ethers.getSigners();
+
+    TestLiquidationStrategy = await ethers.getContractFactory(
+      "TestLiquidationStrategy"
+    );
+    tokenA = await TestERC20.deploy("Test Token A", "TOKA");
+    tokenB = await TestERC20.deploy("Test Token B", "TOKB");
+    TestCFMM = await ethers.getContractFactory("TestCFMM");
+
+    cfmm = await TestCFMM.deploy(
+      tokenA.address,
+      tokenB.address,
+      "Test CFMM",
+      "TCFMM"
+    );
+
+    // because Uniswap's CFMM reorders tokenA and tokenB in increasing order
+    const token0addr = await cfmm.token0();
+    const token1addr = await cfmm.token1();
+    tokenA = await TestERC20.attach(token0addr);
+    tokenB = await TestERC20.attach(token1addr);
+
+    liquidationStrategy = await TestLiquidationStrategy.deploy();
+    await (
+      await liquidationStrategy.initialize(cfmm.address, PROTOCOL_ID, [
+        tokenA.address,
+        tokenB.address,
+      ])
+    ).wait();
+  });
+
+  const depositToStrategy = async (startLpTokens: BigNumber) => {
+    const ONE = BigNumber.from(10).pow(18);
+    const startLiquidity = ONE.mul(800);
+    const lastCFMMInvariant = startLiquidity.mul(2);
+    const lastCFMMTotalSupply = startLpTokens.mul(2);
+    await (
+      await liquidationStrategy.setLPTokenBalance(
+        startLiquidity,
+        startLpTokens,
+        lastCFMMInvariant,
+        lastCFMMTotalSupply
+      )
+    ).wait();
+
+    await (await cfmm.mint(startLpTokens, liquidationStrategy.address)).wait();
+  };
+
+  const borrowMostOfIt = async (amount: BigNumber) => {
+    const res = await (await liquidationStrategy.createLoan()).wait();
+    const tokenId = res.events[0].args.tokenId;
+    await liquidationStrategy.testOpenLoan(tokenId, amount);
+  };
+
+  describe("Test _liquidate", function () {
+    it("returns error HasMargin", async function () {
+      const res = await (await liquidationStrategy.createLoan()).wait();
+      const tokenId = res.events[0].args.tokenId;
+      await expect(
+        liquidationStrategy._liquidate(tokenId, false, [0, 0])
+      ).to.be.revertedWith("HasMargin");
+    });
+
+    it("does not have enough margin so it liquidates", async function () {
+      await depositToStrategy(FOUR);
+      const res = await (await liquidationStrategy.createLoan()).wait();
+      const tokenId = res.events[0].args.tokenId;
+      await (await liquidationStrategy.testOpenLoan(tokenId, ONE)).wait();
+      await borrowMostOfIt(TWO); // spike up interest
+      const startLiquidity = ONE.mul(800);
+      const startLpTokens = ONE.mul(400);
+      const loanLiquidity = ONE.mul(20);
+      const loanLPTokens = ONE.mul(10);
+      const lastCFMMInvariant = startLiquidity.mul(2);
+      const lastCFMMTotalSupply = startLpTokens.mul(2);
+      await (
+        await liquidationStrategy.setLPTokenLoanBalance(
+          tokenId,
+          startLiquidity,
+          startLpTokens,
+          loanLiquidity,
+          loanLPTokens,
+          lastCFMMInvariant,
+          lastCFMMTotalSupply
+        )
+      ).wait();
+      await liquidationStrategy._liquidate(tokenId, false, [0, 0]);
+    });
+  });
+
+  describe("Test _liquidateWithLP", function () {
+    it("returns error HasMargin", async function () {
+      const res = await (await liquidationStrategy.createLoan()).wait();
+      const tokenId = res.events[0].args.tokenId;
+      await expect(
+        liquidationStrategy._liquidateWithLP(tokenId)
+      ).to.be.revertedWith("HasMargin");
+    });
+
+    it("does not have enough margin so it liquidates", async function () {
+      await depositToStrategy(ONE.mul(500));
+      const res = await (await liquidationStrategy.createLoan()).wait();
+      const tokenId = res.events[0].args.tokenId;
+      await (await liquidationStrategy.testOpenLoan(tokenId, ONE)).wait();
+      await borrowMostOfIt(TWO); // spike up interest
+      const startLiquidity = ONE.mul(800);
+      const startLpTokens = ONE.mul(100);
+      const loanLiquidity = ONE.mul(20000);
+      const loanLPTokens = ONE.mul(10);
+      const lastCFMMInvariant = startLiquidity.mul(2);
+      const lastCFMMTotalSupply = startLpTokens.mul(2);
+      await (
+        await liquidationStrategy.setLPTokenLoanBalance(
+          tokenId,
+          startLiquidity,
+          startLpTokens,
+          loanLiquidity,
+          loanLPTokens,
+          lastCFMMInvariant,
+          lastCFMMTotalSupply
+        )
+      ).wait();
+      await (await cfmm.mint(ONE.mul(200), owner.address)).wait();
+      await liquidationStrategy._liquidateWithLP(tokenId);
+    });
+  });
+});
