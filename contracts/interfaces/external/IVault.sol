42d55cc9e094fc860174a6733ded5c4176ffe39f
0xHessian <hessian_a@protonmail.com>
2023-01-05 15:20:34 +0000

    Add `joinPool` to `IVault`

--- contracts/interfaces/external/IVault.sol
+++ contracts/interfaces/external/IVault.sol
@@ -4,7 +4,16 @@ pragma solidity 0.8.4;
 // Interface for the Balancer Vault contract
 // E.g. https://etherscan.io/address/0xBA12222222228d8Ba445958a75a0704d566BF2C8
 
+struct JoinPoolRequest {
+    address[] assets;
+    uint[] maxAmountsIn;
+    bytes userData;
+    bool fromInternalBalance;
+}
+
 interface IVault {
     // Fetches the pool assets held in the vault
     function getPoolTokens(bytes32 poolId) external returns (address[] memory tokens, uint[] memory balances);
+
+    function joinPool(bytes32 poolId, address sender, address recipient, JoinPoolRequest memory request)  external payable;
 }
