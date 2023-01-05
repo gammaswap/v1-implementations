16ffb7edc35f79c0fd2445998b6b87a4414dcebf
0xHessian <hessian_a@protonmail.com>
2023-01-05 12:56:07 +0000

    `TestBalancerShortStrategy` initial commit

---
+++ contracts/test/strategies/balancer/TestBalancerShortStrategy.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.4;
+
+import "../../../strategies/balancer/BalancerShortStrategy.sol";
+
+contract TestBalancerShortStrategy is BalancerShortStrategy {
+
+    using LibStorage for LibStorage.Storage;
+
+    constructor(uint64 _baseRate, uint80 _factor, uint80 _maxApy)
+        BalancerShortStrategy(_baseRate, _factor, _maxApy) {
+    }
+
+    function initialize(address cfmm, address[] calldata tokens, uint8[] calldata decimals) external virtual {
+        s.initialize(msg.sender, cfmm, tokens, decimals);
+    }
+
+    function testCalcDeposits(uint256[] calldata amountsDesired, uint256[] calldata amountsMin) public virtual view returns(uint256[] memory amounts, address payee) {
+        (amounts, payee) = calcDepositAmounts(amountsDesired, amountsMin);
+    }
+
+    function testCheckOptimalAmt(uint256 amountOptimal, uint256 amountMin) public virtual pure returns(uint8){
+        checkOptimalAmt(amountOptimal, amountMin);
+        return 3;
+    }
+
+    function testGetReserves(address cfmm) public virtual view returns(uint128[] memory reserves){
+        return getReserves(cfmm);
+    }
+}
