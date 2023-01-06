0409d3c70ae8a645ea558e8fa2547650a1d63034
0xHessian <hessian_a@protonmail.com>
2023-01-06 20:44:46 +0000

    Update `IWeightedPool2Tokens` with `view` keywords

--- contracts/interfaces/external/IWeightedPool2Tokens.sol
+++ contracts/interfaces/external/IWeightedPool2Tokens.sol
@@ -6,17 +6,17 @@ pragma solidity 0.8.4;
 
 interface IWeightedPool2Tokens {
     // Fetches the pool weights for both assets
-    function getNormalizedWeights() external returns (uint weight0, uint weight1);
+    function getNormalizedWeights() external view returns (uint weight0, uint weight1);
     
     // Fetches the pool current invariant
-    function getInvariant() external returns (uint invariant);
+    function getInvariant() external view returns (uint invariant);
 
     // Fetches the pool previous invariant
-    function getLastInvariant() external returns (uint invariant);
+    function getLastInvariant() external view returns (uint invariant);
 
     // Fetches the swap fee percentage for the pool
-    function getSwapFeePercentage() external returns (int swapFeePercentage);
+    function getSwapFeePercentage() external view returns (int swapFeePercentage);
 
     // Fetches the pool ID
-    function getPoolId() external returns (bytes32 poolId);
+    function getPoolId() external view returns (bytes32 poolId);
 }
