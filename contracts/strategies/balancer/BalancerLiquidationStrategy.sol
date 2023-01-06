e1396ab8d46245031f0ef9b381a2a32b00e56111
0xHessian <hessian_a@protonmail.com>
2023-01-06 21:23:41 +0000

    `BalancerLiquidationStrategy` initial commit

---
+++ contracts/strategies/balancer/BalancerLiquidationStrategy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.4;
+
+import "../base/LiquidationStrategy.sol";
+import "./BalancerBaseLongStrategy.sol";
+
+contract BalancerLiquidationStrategy is BalancerBaseLongStrategy, LiquidationStrategy {
+
+    constructor(uint16 _tradingFee, uint64 _baseRate, uint80 _factor, uint80 _maxApy, address _vault)
+        BalancerBaseLongStrategy(0, _tradingFee, _baseRate, _factor, _maxApy, _vault) {
+    }
+}
