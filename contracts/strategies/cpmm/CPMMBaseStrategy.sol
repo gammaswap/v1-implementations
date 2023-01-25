62ca63481f12294e5f265cc552545f2a57a2ed5e
Daniel Alcarraz <dam417@nyu.edu>
2023-01-25 12:08:56 -0500

    adding natspec to CPMMBaseStrategy, CPMMLiquidationStrategy, CPMMLongStrategy, and CPMMShortStrategy

--- contracts/strategies/cpmm/CPMMBaseStrategy.sol
+++ contracts/strategies/cpmm/CPMMBaseStrategy.sol
@@ -2,48 +2,61 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import "../../interfaces/external/ICPMM.sol";
 import "../../rates/LogDerivativeRateModel.sol";
 import "../base/BaseStrategy.sol";
 
+/// @title Base Strategy implementation contract for Constant Product Market Maker
+/// @author Daniel D. Alcarraz
+/// @notice Common functions used by all concrete strategy implementations for Constant Product Market Maker
+/// @dev This implementation was specifically designed to work with UniswapV2. Inherits Rate Model
 abstract contract CPMMBaseStrategy is BaseStrategy, LogDerivativeRateModel {
 
     error MaxTotalApy();
 
+    /// @dev Number of blocks network will issue within a ear. Currently expected
     uint256 immutable public BLOCKS_PER_YEAR; // 2628000 blocks per year in ETH mainnet (12 seconds per block)
+
+    /// @dev Max total annual APY the GammaPool will charge liquidity borrowers (e.g. 1,000%).
     uint256 immutable public MAX_TOTAL_APY;
 
+    /// @dev Initializes the contract by setting `_maxTotalApy`, `_blocksPerYear`, `_baseRate`, `_factor`, and `_maxApy`
     constructor(uint256 _maxTotalApy, uint256 _blocksPerYear, uint64 _baseRate, uint80 _factor, uint80 _maxApy) LogDerivativeRateModel(_baseRate, _factor, _maxApy) {
-        if(_maxTotalApy < _maxApy) {
+        if(_maxTotalApy < _maxApy) { // maxTotalApy (CFMM Fees + GammaSwap interest rate) cannot be greater or equal to maxApy (max GammaSwap interest rate)
             revert MaxTotalApy();
         }
         MAX_TOTAL_APY = _maxTotalApy;
         BLOCKS_PER_YEAR = _blocksPerYear;
     }
 
+    /// @dev See {BaseStrategy-maxTotalApy}.
     function maxTotalApy() internal virtual override view returns(uint256) {
         return MAX_TOTAL_APY;
     }
 
+    /// @dev See {BaseStrategy-blocksPerYear}.
     function blocksPerYear() internal virtual override view returns(uint256) {
         return BLOCKS_PER_YEAR;
     }
 
+    /// @dev See {BaseStrategy-updateReserves}.
     function updateReserves(address cfmm) internal virtual override {
         (s.CFMM_RESERVES[0], s.CFMM_RESERVES[1],) = ICPMM(cfmm).getReserves();
     }
 
+    /// @dev See {BaseStrategy-depositToCFMM}.
     function depositToCFMM(address cfmm, uint256[] memory, address to) internal virtual override returns(uint256) {
         return ICPMM(cfmm).mint(to);
     }
 
+    /// @dev See {BaseStrategy-withdrawFromCFMM}.
     function withdrawFromCFMM(address cfmm, address to, uint256 amount) internal virtual override returns(uint256[] memory amounts) {
         GammaSwapLibrary.safeTransfer(IERC20(cfmm), cfmm, amount);
         amounts = new uint256[](2);
         (amounts[0], amounts[1]) = ICPMM(cfmm).burn(to);
     }
 
+    /// @dev See {BaseStrategy-calcInvariant}.
     function calcInvariant(address, uint128[] memory amounts) internal virtual override view returns(uint256) {
         return Math.sqrt(uint256(amounts[0]) * amounts[1]);
     }
--- contracts/strategies/cpmm/CPMMLiquidationStrategy.sol
+++ contracts/strategies/cpmm/CPMMLiquidationStrategy.sol
@@ -4,15 +4,22 @@ pragma solidity 0.8.4;
 import "../base/LiquidationStrategy.sol";
 import "./CPMMLongStrategy.sol";
 
+/// @title Liquidation Strategy implementation contract for Constant Product Market Maker
+/// @author Daniel D. Alcarraz
+/// @notice Sets up variables used by LiquidationStrategy and defines internal functions specific to CPMM implementation
+/// @dev This implementation was specifically designed to work with UniswapV2
 contract CPMMLiquidationStrategy is CPMMBaseLongStrategy, LiquidationStrategy {
 
+    /// @return LIQUIDATION_FEE_THRESHOLD - 1 - feeThreshold % = liquidation penalty charged at collateral
     uint16 immutable public LIQUIDATION_FEE_THRESHOLD;
 
+    /// @dev Initializes the contract by setting `_liquidationThreshold`, `_liquidationFeeThreshold`, `_maxTotalApy`, `_blocksPerYear`, `_tradingFee1`, `_tradingFee2`, `_baseRate`, `_factor`, and `_maxApy`
     constructor(uint16 _liquidationThreshold, uint16 _liquidationFeeThreshold, uint256 _maxTotalApy, uint256 _blocksPerYear, uint16 _tradingFee1, uint16 _tradingFee2, uint64 _baseRate, uint80 _factor, uint80 _maxApy)
         CPMMBaseLongStrategy(_liquidationThreshold, _maxTotalApy, _blocksPerYear, 0, _tradingFee1, _tradingFee2, _baseRate, _factor, _maxApy) {
         LIQUIDATION_FEE_THRESHOLD = _liquidationFeeThreshold;
     }
 
+    /// @dev returns liquidation fee threshold
     function liquidationFeeThreshold() internal virtual override view returns(uint16) {
         return LIQUIDATION_FEE_THRESHOLD;
     }
