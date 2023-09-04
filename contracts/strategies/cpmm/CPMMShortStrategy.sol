7a6f90def0d74de5a75d5092efefa5881a74d573
danielalcarraz <dam417@nyu.edu>
2023-09-04 11:26:31 -0400

    downgrade solidity compiler to version 0.8.19

--- contracts/strategies/cpmm/CPMMShortStrategy.sol
+++ contracts/strategies/cpmm/CPMMShortStrategy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.21;
+pragma solidity 0.8.19;
 
 import "@gammaswap/v1-core/contracts/strategies/ShortStrategySync.sol";
 import "../../interfaces/external/cpmm/ICPMM.sol";
--- contracts/test/strategies/cpmm/TestCPMMLiquidationStrategy.sol
+++ contracts/test/strategies/cpmm/TestCPMMLiquidationStrategy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.21;
+pragma solidity 0.8.19;
 
 import "@gammaswap/v1-core/contracts/strategies/base/BaseBorrowStrategy.sol";
 import "../../../strategies/cpmm/liquidation/CPMMLiquidationStrategy.sol";
