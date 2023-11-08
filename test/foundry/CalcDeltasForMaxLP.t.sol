58ec96e71cee3c1d0d4246d1bf461d219f7c2fe7
danielalcarraz <dam417@nyu.edu>
2023-11-08 02:32:02 -0500

    improve calcDeltasForMaxLP unit tests

--- test/foundry/CalcDeltasForMaxLP.t.sol
+++ test/foundry/CalcDeltasForMaxLP.t.sol
@@ -13,10 +13,10 @@ contract CalcDeltasForMaxLP is CalcDeltasBase {
         uint256 rightVal = uint256(reserve1) * uint256(tokensHeld0);
 
         if(leftVal > rightVal) {
-            deltas = mathLib.calcDeltasForMaxLP(tokensHeld0, tokensHeld1, reserve0, reserve1, 997, 1000, decimals0);
+            deltas = mathLib.calcDeltasForMaxLP(tokensHeld0, tokensHeld1, reserve0, reserve1, fee1, fee2, decimals0);
             (deltas[0], deltas[1]) = (deltas[1], 0); // swap result, 1st root (index 0) is the only feasible trade
         } else if(leftVal < rightVal) {
-            deltas = mathLib.calcDeltasForMaxLP(tokensHeld1, tokensHeld0, reserve1, reserve0, 997, 1000, decimals0);
+            deltas = mathLib.calcDeltasForMaxLP(tokensHeld1, tokensHeld0, reserve1, reserve0, fee1, fee2, decimals0);
             (deltas[0], deltas[1]) = (0, deltas[1]); // swap result, 1st root (index 0) is the only feasible trade
         }
 
@@ -25,29 +25,76 @@ contract CalcDeltasForMaxLP is CalcDeltasBase {
 
         if(deltas[0] > 0 || deltas[1] > 0) {
             assertGe(GSMath.sqrt(uint256(tokensHeld0) * tokensHeld1), collateral0); // when rebalancing is small it might not pass
-            uint256 cfmmRatio = uint256(reserve1) * (10**decimals1) / uint256(reserve0);
-            uint256 tokenRatio = uint256(tokensHeld1) * (10**decimals1) / uint256(tokensHeld0);
-            bool checkRatio = tokensHeld0 >= 1e12 && tokensHeld1 >= 1e12 && reserve0 >= 1e12 && reserve1 >= 1e12;
+            bool checkRatio = tokensHeld0 >= (10**decimals0) && tokensHeld1 >= (10**decimals1) &&
+                reserve0 >= (10**decimals0) && reserve1 >= (10**decimals1);
             if(checkRatio && precision <= 1e18) {
-                cfmmRatio = cfmmRatio/(10**decimals1);
-                tokenRatio = tokenRatio/(10**decimals1);
+                uint256 cfmmRatio = uint256(reserve1) * (10**decimals0) / uint256(reserve0);
+                uint256 tokenRatio = uint256(tokensHeld1) * (10**decimals0) / uint256(tokensHeld0);
                 uint256 diff = cfmmRatio > tokenRatio ? cfmmRatio - tokenRatio : tokenRatio - cfmmRatio;
                 assertEq(diff/precision,0);
             }
         }
     }
 
+    function rebalanceToCFMMCasesByReserves(uint128 tokensHeld0, uint128 tokensHeld1,
+        uint8[] memory decimals, uint256 precision) internal {
+        // token = cfmm
+        uint128 reserve0 = tokensHeld0 * 100;//10_000 * 1e18;
+        uint128 reserve1 = tokensHeld1 * 100;//1_000_000 * 1e18;
+        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, decimals[0], decimals[1], precision);
+
+        // token < cfmm
+        reserve0 = tokensHeld0 * 100; //10_000 * 1e18;
+        reserve1 = tokensHeld1 * 100 * 200; //2_000_000 * 1e18;
+        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, decimals[0], decimals[1], precision);
+
+        // token > cfmm
+        reserve0 = tokensHeld0 * 100 * 200; //2_000_000 * 1e18;
+        reserve1 = tokensHeld1 * 100; //10_000 * 1e18;
+        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, decimals[0], decimals[1], precision);/**/
+    }
+
+    function testRebalanceToCFMMRatioFixed() public {
+        uint8[] memory decimals = new uint8[](2);
+        decimals[0] = 18;
+        decimals[1] = 18;
+
+        uint128 tokensHeld0 = uint128(10_000 * (10**decimals[0])); // 100
+        uint128 tokensHeld1 = uint128(1_000_000 * (10**decimals[1])); // 10_000
+
+        rebalanceToCFMMCasesByReserves(tokensHeld0, tokensHeld1, decimals, 1e2);
+    }
+
+    function testRebalanceToCFMMRatioFixed6x18() public {
+        uint8[] memory decimals = new uint8[](2);
+        decimals[0] = 6;
+        decimals[1] = 18;
+
+        uint128 tokensHeld0 = uint128(10_000 * (10**decimals[0])); // 100
+        uint128 tokensHeld1 = uint128(1_000_000 * (10**decimals[1])); // 10_000
+
+        rebalanceToCFMMCasesByReserves(tokensHeld0, tokensHeld1, decimals, 1e13);
+    }
+
+    function testRebalanceToCFMMRatioFixed18x6() public {
+        uint8[] memory decimals = new uint8[](2);
+        decimals[0] = 18;
+        decimals[1] = 6;
+
+        uint128 tokensHeld0 = uint128(10_000 * (10**decimals[0])); // 100
+        uint128 tokensHeld1 = uint128(1_000_000 * (10**decimals[1])); // 10_000
+
+        rebalanceToCFMMCasesByReserves(tokensHeld0, tokensHeld1, decimals, 1e0);
+    }
+
     function testRebalanceToCFMMRatio(uint112 _reserve0, uint112 _reserve1, uint8 borrow, uint8 move, bool side) public {
         if(move < 128) move = 128;
 
         (uint128 reserve0, uint128 reserve1, uint128 tokensHeld0, uint128 tokensHeld1) =
-            createMarketPosition(_reserve0, _reserve1, borrow, move, side);
+            createMarketPosition2(_reserve0, _reserve1, borrow, move, side);
 
-        uint256 cfmmRatio = uint256(reserve1) * (10**18) / uint256(reserve0);
-        uint256 tokenRatio = uint256(tokensHeld1) * (10**18) / uint256(tokensHeld0);
-
-        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, 18, 18, 1e8);
-        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, 6, 18, 1e10);
-        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, 18, 6, 1e10);
+        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, 18, 18, 1e10);
+        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, 6, 18, 1e4);
+        rebalanceToCFMM(tokensHeld0, tokensHeld1, reserve0, reserve1, 18, 6, 1e8);
     }
 }
