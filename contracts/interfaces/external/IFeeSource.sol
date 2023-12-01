c303c5b5f098f83313e7f58157e0839bca4c2391
danielalcarraz <dam417@nyu.edu>
2023-12-01 12:54:46 -0500

    IFeeSource natspec

--- contracts/interfaces/external/IFeeSource.sol
+++ contracts/interfaces/external/IFeeSource.sol
@@ -1,5 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
+/// @title Interface for contract that has fee information for transacting with AMM
+/// @author Daniel D. Alcarraz (https://github.com/0xDanr)
+/// @notice Used primarily with DeltaSwap
 interface IFeeSource {
+    /// @dev Get fee charged to GammaSwap from feeSource contract in basis points (e.g. 3 = 3 basis points)
     function gsFee() external view returns(uint8);
 }
